443bd28762c4664e9438f1df202f050f8169b916
Sab Ben <sbenseghir.sb@gmail.com>
2025-11-24 21:06:18 +0100

    First commit

---
+++ src/test/java/com/example/eventbackend/EventBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
