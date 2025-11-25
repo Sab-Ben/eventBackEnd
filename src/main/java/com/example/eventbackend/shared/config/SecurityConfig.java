fc433f7955ce6f548b825230c413a551e5a2d0af
Sab Ben <sbenseghir.sb@gmail.com>
2025-11-25 15:32:50 +0100

    Création des packages

--- src/main/java/com/example/eventbackend/shared/config/SecurityConfig.java
+++ src/main/java/com/example/eventbackend/shared/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.eventbackend.config;
+package com.example.eventbackend.shared.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
