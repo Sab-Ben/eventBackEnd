815737e267bec84aeb2a9fc0ec96f23129025c09
Sab Ben <sbenseghir.sb@gmail.com>
2025-11-25 09:28:30 +0100

    Finalisation configuration

--- src/main/java/com/application/eventbackend/EventBackEndApplication.java
+++ src/main/java/com/application/eventbackend/EventBackEndApplication.java
@@ -1,4 +1,4 @@
-package com.example.eventbackend;
+package com.application.eventbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/application/eventbackend/EventBackEndApplicationTests.java
+++ src/test/java/com/application/eventbackend/EventBackEndApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.eventbackend;
+package com.application.eventbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
