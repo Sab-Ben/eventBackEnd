e94b50f2d3297ae7a805237b722715f1300ba39d
Sab Ben <sbenseghir.sb@gmail.com>
2025-11-26 12:28:12 +0100

    feat/fix(develop): Suppression fichiers de Booking et Social

--- src/main/java/com/example/eventbackend/booking/application/BookingNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.eventbackend.booking.application;
-
-import java.util.UUID;
-
-public class BookingNotFoundException extends RuntimeException {
-    public BookingNotFoundException(UUID id) {
-        super("Booking not found: " + id);
-    }
-}
--- src/main/java/com/example/eventbackend/catalog/application/EventNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.eventbackend.catalog.application;
-
-public class EventNotFoundException extends RuntimeException {
-    public EventNotFoundException(String id) {
-        super("Event not found: " + id);
-    }
-}
